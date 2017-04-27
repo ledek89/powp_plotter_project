452d5b236651a7a004c781efaade9cfd9e45dd39
Błażej Lewandowski <ledek89@gmail.com>
2017-04-27 15:25:18 +0200

    3.1.3

--- src/main/java/edu/iis/powp/command/PlotterCommand.java
+++ src/main/java/edu/iis/powp/command/PlotterCommand.java
@@ -2,6 +2,8 @@ package edu.iis.powp.command;
 
 import edu.iis.client.plottermagic.IPlotter;
 
+//Chodzi o wzorzec kompozyt
+
 public interface PlotterCommand {
 
 	public void execute(IPlotter driver);
